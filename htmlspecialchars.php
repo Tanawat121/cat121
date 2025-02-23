1ea61ca35ae5aa01eb06f8e79d000f934bd166ad
Tanawat121 <balltheph12@gmail.com>
2025-02-23 22:44:21 +0700

    Create htmlspecialchars.php

---
+++ htmlspecialchars.php
@@ -0,0 +1,3 @@
+<td><?= htmlspecialchars($row['name_th'], ENT_QUOTES, 'UTF-8'); ?></td>
+<td><?= htmlspecialchars($row['name_en'], ENT_QUOTES, 'UTF-8'); ?></td>
+<td><?= nl2br(htmlspecialchars($row['description'], ENT_QUOTES, 'UTF-8')); ?></td>
